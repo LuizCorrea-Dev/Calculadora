82eae1a304b1c779e78b79d0f69899751850c077
LuizCorrea-Dev <correa.luizbatista@gmail.com>
2022-03-03 11:05:43 -0300

    Initial commit

---
+++ app/src/main/java/luiz/correa/calculadora/SplashActivity.kt
@@ -0,0 +1,28 @@
+package luiz.correa.calculadora
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        supportActionBar?.hide()
+        Handler(Looper.getMainLooper()).postDelayed({openMainActivity()}, 1500)
+    }
+
+    private fun openMainActivity() {
+        startActivity(Intent(this, MainActivity::class.java))
+        finish()
+    }
+
+}
